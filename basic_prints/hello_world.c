8b2220bf91d051bccfb30c3d0955e956ed04dcee
CharaD7 <jijakahn6@gmail.com>
2022-07-30 13:56:06 +0000

    Print Welcome to this world program

---
+++ basic_prints/hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The main body of the program
+ *
+ * return 0 : Always(0)
+ */
+
+int main() {
+  // Print a basic string "Welcome to this World"
+  printf("Welcome to this World!");
+
+  return 0;
+}
